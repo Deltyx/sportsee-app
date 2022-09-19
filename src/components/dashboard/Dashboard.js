b2fe4aadafa611de8ca39c917f5ae3ff7ae9b792
Alexis <alexismorin.pro@gmail.com>
2022-09-19 17:25:50 +0200

    Added RadialChart (Static)

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,8 +1,9 @@
 import './Dashboard.scss';
 
 import Sidebar from '../sidebar/Sidebar.js';
-import Skills_Chart from '../radarchart/Skills_Chart.js';
-import Session_Duration_Chart from '../linechart/Session_Duration_Chart.js';
+import Skills_Chart from '../skills_chart/Skills_Chart.js';
+import Session_Duration_Chart from '../session_duration_chart/Session_Duration_Chart.js';
+import Score_Chart from '../score_chart/Score_Chart';
 
 
 export default function Dashboard() {
@@ -12,6 +13,7 @@ export default function Dashboard() {
       <div className='dashboard-charts-container'>
         <Session_Duration_Chart />
         <Skills_Chart />
+        <Score_Chart />
       </div>
     </section>
   );
---
+++ src/components/score_chart/Score_Chart.js
@@ -0,0 +1,52 @@
+import React, { PureComponent } from 'react';
+import { RadialBarChart, RadialBar, ResponsiveContainer } from 'recharts';
+
+import './Score_Chart.scss';
+
+const data = [
+  {
+    score: 31.47,
+    pv: 2400,
+    fill: '#FF0000',
+  },
+];
+
+const style = {
+  top: '50%',
+  right: 0,
+  transform: 'translate(0, -50%)',
+  lineHeight: '24px',
+};
+
+export default class Score_Chart extends PureComponent {
+
+  render() {
+    return (
+    <div className='Score_Chart-wrapper'>
+        <h2 className='Score_Chart-title'>Score</h2>
+        <ResponsiveContainer width="100%" height="100%">
+            <RadialBarChart 
+                cx="50%" 
+                cy="50%" 
+                innerRadius="70%"  
+                data={data}
+                startAngle={90} 
+                endAngle={450}
+            >
+            <RadialBar
+                minAngle={15}
+                background
+                cornerRadius={25}
+                barSize={8}
+                dataKey="score"
+            />
+            </RadialBarChart>
+        </ResponsiveContainer>
+        <div className="Score_Chart-score-wrapper">
+            <p className="Score_Chart-score">100%</p>
+            <p className="Score_Chart-score-text">de votre<br/>objectif</p>
+        </div>
+    </div>
+    );
+  }
+}
