2aa9df2a71e71006abc1e8758f5dd07f53158278
Alexis <alexismorin.pro@gmail.com>
2022-10-12 14:50:14 +0200

    Finished JSDOC

--- src/services/userInfoBuilder.js
+++ src/services/userInfoBuilder.js
@@ -1,4 +1,11 @@
+/** Class representing a user's infos. */
 class userInfos {
+    /**
+        * Create user's infos.
+        * @param {string} firstName - The user's firstName.
+        * @param {string} lastName - The user's lastName.
+        * @param {number} age - The user's age.
+    */
     constructor(firstName = "", lastName = "", age = 0) {
         this.firstName = firstName
         this.lastName = lastName
@@ -6,7 +13,15 @@ class userInfos {
     }
 }
 
+/** Class representing a user's keyData. */
 class keyData {
+    /**
+        * Create user's keyData.
+        * @param {number} calorieCount - The user's calorieCount.
+        * @param {number} proteinCount - The user's proteinCount.
+        * @param {number} carbohydrateCount - The user's carbohydrateCount.
+        * @param {number} lipidCount - The user's lipidCount.
+    */
     constructor(calorieCount = 0, proteinCount = 0, carbohydrateCount = 0, lipidCount = 0) {
         this.calorieCount = calorieCount
         this.proteinCount = proteinCount
@@ -15,7 +30,15 @@ class keyData {
     }
 }
 
+/** Class representing a user. */
 class userInfo {
+    /**
+        * Create a user.
+        * @param {number} id - The user's id.
+        * @param {object} userInfos - The user's infos.
+        * @param {number} score - The user's score.
+        * @param {object} keyData - The user's keyData.
+    */
     constructor(id = 0, userInfos = null, score = 0, keyData = null) {
         this.id = id
         this.userInfos = userInfos
@@ -24,31 +47,59 @@ class userInfo {
     }
 }
 
+/** Class creating a user. */
 export class userInfoBuilder {
+    /**
+        * Create a user.
+    */
     constructor() {
         this.userInfo = new userInfo()
     }
 
+    /**
+        * Set a user ID.
+        * @param {number} value - The user's id fetched from API.
+        * @return the builder
+    */
     setId(value) {
         this.userInfo.id = value
         return this
     }
 
+    /**
+        * Set a user's infos.
+        * @param {object} value - The user's infos fetched from API.
+        * @return the builder
+    */
     setUserInfos(value) {
         this.userInfo.userInfos = new userInfos(value.firstName, value.lastName, value.age)
         return this
     }
 
+    /**
+        * Set a user's score.
+        * @param {number} value - The user's score fetched from API.
+        * @return the builder
+    */
     setScore(value) {
         this.userInfo.score = value
         return this
     }
 
+    /**
+        * Set a user's keyData.
+        * @param {object} value - The user's keyData fetched from API.
+        * @return the builder
+    */
     setKeyData(value) {
         this.userInfo.keyData = new keyData(value.calorieCount, value.proteinCount, value.carbohydrateCount, value.lipidCount)
         return this
     }
 
+    /**
+        * Build the user object.
+        * @return a user with the correct data set.
+    */ 
     build() {
         return this.userInfo
     }
